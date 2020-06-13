54e807f671eb49359b8797aa4f3c1f2c336a7b9c
Dyanjun <dyanjun15201981339@163.com>
2020-06-13 15:10:15 +0800

    delete

--- FuturesTrading_Backend/traderGateway-service/src/main/java/com/example/traderservicedemo/core/processor/Processor.java
+++
@@ -1,9 +0,0 @@
-package com.example.traderservicedemo.core.processor;
-
-import com.example.traderservicedemo.model.Order;
-
-import java.util.List;
-
-public interface Processor {
-    List<Order> process(Order order);
-}
