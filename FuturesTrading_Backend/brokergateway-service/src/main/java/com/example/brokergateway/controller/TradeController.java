610127f52ba40f1ce313c8922c501838ad1e90e3
zhaoyang110157 <1853935753@qq.com>
2020-06-15 02:20:19 +0800

    fix tradeController

--- FuturesTrading_Backend/brokergateway-service/src/main/java/com/example/brokergateway/controller/TradeController.java
+++ FuturesTrading_Backend/brokergateway-service/src/main/java/com/example/brokergateway/controller/TradeController.java
@@ -5,6 +5,7 @@ import com.example.brokergateway.server.TradeServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,12 +17,12 @@ public class TradeController {
     public TradeServer tradeServer;
 
     @GetMapping("/trade/getByTrader")
-    public List<Trade> getByTrader(Integer trader_id){
+    public List<Trade> getByTrader(@RequestParam Integer trader_id){
         return tradeServer.getByTrader(trader_id);
     }
 
     @GetMapping("/trade/getByBroker")
-    public List<Trade> getByBroker(Integer trader_id){
+    public List<Trade> getByBroker(@RequestParam Integer trader_id){
         return tradeServer.getByBroker(trader_id);
     }
 }
