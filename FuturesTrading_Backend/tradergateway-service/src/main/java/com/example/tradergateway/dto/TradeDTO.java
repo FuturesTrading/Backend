7c5deaa12236cc4ad38800678ecbc7eac7093a92
Dyanjun <dyanjun15201981339@163.com>
2020-06-15 22:19:52 +0800

    update

--- FuturesTrading_Backend/tradergateway-service/src/main/java/com/example/tradergateway/dto/TradeDTO.java
+++ FuturesTrading_Backend/tradergateway-service/src/main/java/com/example/tradergateway/dto/TradeDTO.java
@@ -2,18 +2,21 @@ package com.example.tradergateway.dto;
 
 import lombok.Data;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.sql.Timestamp;
 
 @Data
 public class TradeDTO {
-    Integer tradeId;
-    Integer brokerId;
-    Integer buyerId;
-    Integer sellerId;
-    Timestamp time;
-    Integer productId;
-    Integer quantity;
-    Integer direction;
-    Float commissionCount;
+    private Integer tradeId;
+    private Integer brokerId;
+    private Integer buyerId;
+    private Integer sellerId;
+    private Timestamp time;
+    private Integer productId;
+    private Integer quantity;
+    private Boolean direction;
+    private Float commissionCount;
+    private Float price;
 }
